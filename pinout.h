e6eee932d6c3f76b8a085c09b2558b366aee810e
Daniel Shamany <dshamany@proton.me>
2025-12-06 00:28:59 +0100

    adjust pins to suit ESP32-WROVER with ext. board

--- pinout.h
+++ pinout.h
@@ -1,15 +1,16 @@
 #pragma once
 
-const uint8_t PIN_COL_1 = 32;
-const uint8_t PIN_COL_2 = 26;
-const uint8_t PIN_COL_3 = 100;
+const uint8_t RGB_RED = 23;
+const uint8_t RGB_GRN = 22;
+const uint8_t RGB_BLU = 21;
+
+const uint8_t PIN_COL_1 = 0;
+const uint8_t PIN_COL_2 = 2;
+const uint8_t PIN_COL_3 = 15;
 
 const uint8_t PIN_ROW_1 = 4;
-const uint8_t PIN_ROW_2 = 19;
-const uint8_t PIN_ROW_3 = 100;
+const uint8_t PIN_ROW_2 = 5;
+const uint8_t PIN_ROW_3 = 18;
+const uint8_t PIN_ROW_4 = 19;
 
-const uint8_t BUZZ_PIN = 33;
-
-const uint8_t RGB_RED = 21;
-const uint8_t RGB_GRN = 22;
-const uint8_t RGB_BLU = 23;
+const uint8_t BUZZ_PIN = 13;
