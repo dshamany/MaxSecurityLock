83861a95b1bc2ea1ee540c3f1ef7867b5ae4a4f5
Daniel Shamany <dshamany@proton.me>
2025-12-06 00:29:15 +0100

    split into smaller functions

--- MaxSecurityLock.ino
+++ MaxSecurityLock.ino
@@ -5,17 +5,13 @@
 #include "KeyLock.h"
 #include "pinout.h"
 
-#define ROWS 2
-#define COLS 2
+#define ROWS 4
+#define COLS 3
 
-char keyMap[ROWS][COLS] = {{'1','2'}, {'3','4'}};
+char keyMap[ROWS][COLS] = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}, {'*','0','#'}};
 
-constexpr uint8_t pinColumn1 = PIN_COL_1;
-constexpr uint8_t pinColumn2 = PIN_COL_2;
-constexpr uint8_t pinRow1 = PIN_ROW_1;
-constexpr uint8_t pinRow2 = PIN_ROW_2;
-uint8_t rowPins[ROWS] = {PIN_ROW_1, PIN_ROW_2};
-uint8_t colPins[COLS] = {PIN_COL_1, PIN_COL_2};
+uint8_t rowPins[ROWS] = {PIN_ROW_1, PIN_ROW_2, PIN_ROW_3, PIN_ROW_4};
+uint8_t colPins[COLS] = {PIN_COL_1, PIN_COL_2, PIN_COL_3};
 Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, ROWS, COLS);
 
 const int passcodeLength = 4;
@@ -29,88 +25,93 @@ enum Mode {
 };
 
 char passcode[PASSCODE_MAX_LENGTH] = { '1', '2', '3', '4', ' ', ' ', ' ', ' ' };
-Mode currentMode = Mode::SET;
+Mode currentMode = Mode::LOCKED;
 KeyLock keylock(passcode, passcodeLength);
 
 bool printedDebug = false;
 
 // Led & Buzzer
-constexpr uint8_t buzzPin = BUZZ_PIN;
-constexpr uint8_t ledPinR = RGB_RED;
-constexpr uint8_t ledPinG = RGB_GRN;
-constexpr uint8_t ledPinB = RGB_BLU;
 unsigned long now;
 unsigned long nextLedTrigger = 0;
 unsigned long ledLoopDuration = 1000;
 
-Buzzer buzzer(buzzPin);
-Led led(ledPinR, ledPinG, ledPinB);
+Buzzer buzzer(BUZZ_PIN);
+Led led(RGB_RED, RGB_GRN, RGB_BLU);
 
 void setup() {
   Serial.begin(115200);
   delay(500);
   Serial.println("BOOT!");
-
-  if (currentMode == Mode::SET) {
-    Serial.println(" ");
-    Serial.println("## Set Password ##");
-  }
 }
 
 static int color = 0;
 
 void loop() {
+
   updateKeypadStates();
   buzzer.update();
   led.update();
-  delay(10);
+}
+
+void clearDigits() {
+  char cleared_digits[PASSCODE_MAX_LENGTH] = {' ',' ',' ',' ',' ',' ', ' ', ' '};
+  memcpy(digits, cleared_digits, PASSCODE_MAX_LENGTH);
+}
+
+void handleKeyEntry() {
+  const bool isMatch = keylock.passcodeMatch(digits);
+  if (currentMode == Mode::LOCKED && isMatch) {
+    currentMode = Mode::UNLOCKED;
+    led.setColorGreen();
+    led.turnOnFor(500);
+    buzzer.turnOnFor(500);
+    Serial.println("** Correct Passcode **");
+  } else if (currentMode == Mode::SET) {
+    keylock.changePasscode(passcode, digits);
+    currentMode = Mode::LOCKED;
+    buzzer.turnOnFor(500);
+    led.turnOff(); // Because the mode sets it to blue
+    Serial.println("## Passcode Set ##");
+  } else {
+    currentMode = Mode::LOCKED;
+    led.setColorRed();
+    led.turnOnFor(200);
+    buzzer.turnOnFor(200);
+    Serial.println("-- Incorrect Passcode --");
+  }
+  clearDigits();
+}
+
+void changePasscode() {
+  currentMode = Mode::SET;
+  led.setColorBlue();
+  led.turnOn();
 }
 
 void updateKeypadStates() {
   char key = keypad.getKey();
+  KeyState state = keypad.getState(); 
+
   if (key) {
-    if (key >= '0' && key <= '9' && passcodeIdx < 10) {
+    Serial.println(key);
+    if (key >= '0' && key <= '9') {
+      buzzer.turnOnFor(100);
+      led.setColorBlue();
+      led.turnOnFor(100);
       digits[passcodeIdx++] = key;
-      // Serial.print("Digit: ");
-      // Serial.println(key);
     } else if (key == '#') {
-      digits[passcodeIdx] = '\0';
-      // Serial.print("Array: ");
-      Serial.println(digits);
+      handleKeyEntry();
       passcodeIdx = 0;
     } else if (key == '*') {
-      passcodeIdx = 0;
-      Serial.println("Cleared");
+      clearDigits();
+      buzzer.turnOnFor(200);
+      led.setColorBlue();
+      led.turnOn();
     }
   }
 
-  switch (currentMode){
-    case Mode::LOCKED:
-      if (passcodeIdx == passcodeLength) {
-        const bool isMatch = keylock.passcodeMatch(digits);
-        if (isMatch) {
-          Serial.println("** Correct Passcode **");
-          buzzer.turnOnFor(500);
-          led.setColorGreen();
-          led.turnOnFor(500);
-        } else {
-          Serial.println("-- Incorrect Passcode --");
-          buzzer.turnOnFor(100);
-          led.setColorRed();
-          led.turnOnFor(100);
-        }
-        passcodeIdx = 0;
-      }
-      break;
-    case Mode::SET:
-      if (passcodeIdx == passcodeLength) {
-        keylock.changePasscode(passcode, digits);
-        currentMode = Mode::LOCKED;
-        Serial.println("## Password Set ##");
-        buzzer.turnOnFor(500);
-        led.setColorBlue();
-        led.turnOnFor(500);
-        passcodeIdx = 0;
-      }
+  if (state == HOLD && currentMode == Mode::UNLOCKED) {
+    changePasscode();
+    passcodeIdx = 0;
   }
 }
