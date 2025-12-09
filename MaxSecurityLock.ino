efb7a68cb2838a666b5d6c8ba0401e55aeb1695f
Daniel Shamany <dshamany@proton.me>
2025-12-09 16:03:43 +0100

    connect to Arduino IoT Cloud

--- MaxSecurityLock.ino
+++ MaxSecurityLock.ino
@@ -1,11 +1,29 @@
+#include "thingProperties.h"
 #include "SystemManager.h"
+
+#ifdef CLOSED
+#undef CLOSED
+#endif
 
 void setup() {
   Serial.begin(115200);
   delay(500);
   Serial.println("BOOT!");
+  SystemManager::setup();
+
+  initProperties();
+
+  ArduinoCloud.addCallback(ArduinoIoTCloudEvent::CONNECT, SystemManager::onCloudConnect);
+  
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
+
 }
 
 void loop() {
+  ArduinoCloud.update();
+  
   SystemManager::run();
 }
