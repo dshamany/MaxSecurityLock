7d7eefa283d3ab1e11bc436141f6edd7d1c2153c
Daniel Shamany <dshamany@proton.me>
2025-12-09 16:04:15 +0100

    updated pins for new wiring

--- pinout.h
+++ pinout.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <Arduino.h>
+
 
 const uint8_t RGB_RED = 14;
 const uint8_t RGB_GRN = 7;
@@ -8,9 +10,9 @@ const uint8_t PIN_COL_1 = 4;
 const uint8_t PIN_COL_2 = 3;
 const uint8_t PIN_COL_3 = 2;
 
-const uint8_t PIN_ROW_1 = 5;
-const uint8_t PIN_ROW_2 = 0;
-const uint8_t PIN_ROW_3 = 1;
-const uint8_t PIN_ROW_4 = 8;
+const uint8_t PIN_ROW_1 = 8;
+const uint8_t PIN_ROW_2 = 1;
+const uint8_t PIN_ROW_3 = 0;
+const uint8_t PIN_ROW_4 = 5;
 
 const uint8_t BUZZ_PIN = 13;
